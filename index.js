d335ab779286c860ced07ff3168aee3824c76278
Chloe Steffens <caforonda103@yahoo.com>
2020-09-29 06:16:49 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,27 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hourInString = time.split(":");
+  const hoursInInt = parseInt(hourInString, 10);
+  
+  if (hoursInInt < 12) {
+    return displayMessage('Good Morning');
+  }
+  
+  else if (hoursInInt >=12 && hoursInInt <= 17){
+    return displayMessage('Good Afternoon');
+  }
+  
+  else {
+    return displayMessage('Good Evening');
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(msg) {
+  const element = document.getElementById("greeting");
+  element.innerText = msg;
+  return msg;
+}
